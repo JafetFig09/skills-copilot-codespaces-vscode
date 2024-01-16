7eb684987c0c40ec8cc7622865b6cd72b590045b
Jafet Figueroa <134158669+JafetFig09@users.noreply.github.com>
2024-01-16 04:28:11 +0000

    Copilot thirt commint

--- member.js
+++ member.js
@@ -1,12 +1,3 @@
 function skillMembers() {
-    return {
-        restrict: 'E',
-        templateUrl: 'app/components/skill-members/skill-members.html',
-        controller: 'SkillMembersController',
-        controllerAs: 'skillMembersCtrl',
-        bindToController: true,
-        scope: {
-            skill: '='
-        }
-    };
+   console.log('skillMembers');
 }
